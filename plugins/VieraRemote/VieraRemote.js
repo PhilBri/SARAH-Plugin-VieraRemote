738c9ebd36d2987d9970713bd4e89add2c04abb6
Phil Bri <ph.brioude@gmail.com>
2014-04-07 19:55:58 +0200

    V1.0

--- plugins/VieraRemote/VieraRemote.js
+++ plugins/VieraRemote/VieraRemote.js
@@ -5,7 +5,7 @@
 |__________________________________________________|
 */
 
-exports.action = function ( data , callback , config , SARAH ){
+exports.action = function ( data , callback , config , SARAH ) {
 
 	cfg = config.modules.vieraremote;
 	if ( !cfg.vieraip ) {
@@ -16,32 +16,32 @@ exports.action = function ( data , callback , config , SARAH ){
 
 	// init
 	var TvIp = cfg.vieraip;
-	var keyArray = data.key.split(',');
+	var keyArray = data.key.split( ',' );
 	
 	sendViera ( keyArray );
 
-	function sendViera ( cmdList ) {
+	function sendViera ( TvCmd ) {
 
 		// making Viera command
-		var cmdViera = cmdList.shift();
+		var cmdViera = keyArray.shift();
 		switch ( cmdViera.substr(0,3) ) {
 			case "NRC" :
-				var TvCode   = '<X_KeyEvent>' + cmdViera + '</X_KeyEvent>';
-				var TvAction = 'X_SendKey';
-				var TvUrl    = '/nrc/control_0';
-				var TvUrn    = 'panasonic-com:service:p00NetworkControl:1#';
+				var TvCode		= '<X_KeyEvent>' + cmdViera + '</X_KeyEvent>';
+				var TvAction	= 'X_SendKey';
+				var TvUrl		= '/nrc/control_0';
+				var TvUrn		= 'panasonic-com:service:p00NetworkControl:1#';
 				break;
 			case "Set" :
-				var TvCode   = '<InstanceID>0</InstanceID><Channel>Master</Channel><DesiredVolume>'+ data.volume.toString() + '</DesiredVolume>';
-				TvAction	 = cmdViera;
-				var	TvUrl    = '/dmr/control_0';
-				var TvUrn    = 'schemas-upnp-org:service:RenderingControl:1#';
+				var TvCode		= '<InstanceID>0</InstanceID><Channel>Master</Channel><DesiredVolume>'+ data.volume.toString() + '</DesiredVolume>';
+				TvAction		= cmdViera;
+				var	TvUrl		= '/dmr/control_0';
+				var TvUrn		= 'schemas-upnp-org:service:RenderingControl:1#';
 				break;
 			case "Get" :
-				var TvCode   = '<InstanceID>0</InstanceID><Channel>Master</Channel>';
-				var TvAction = cmdViera;
-				var	TvUrl    = '/dmr/control_0';
-				var TvUrn    = 'schemas-upnp-org:service:RenderingControl:1#';
+				var TvCode		= '<InstanceID>0</InstanceID><Channel>Master</Channel>';
+				var TvAction	= cmdViera;
+				var	TvUrl		= '/dmr/control_0';
+				var TvUrn		= 'schemas-upnp-org:service:RenderingControl:1#';
 				break;
 			default :
 				callback({ 'tts': 'Erreur dans le fichier X M L' });
@@ -54,7 +54,7 @@ exports.action = function ( data , callback , config , SARAH ){
 			body += 	'<s:Body>'
 			body += 		'<u:'+ TvAction +' xmlns:u="urn:' + TvUrn + '">'
 			body +=				TvCode
-			body += 		'</u:'+ TvAction
+			body += 		'</u:'+ TvAction +">"
 			body += 	'</s:Body>'
 			body += '</s:Envelope>\n\r';
 
@@ -69,17 +69,17 @@ exports.action = function ( data , callback , config , SARAH ){
 				'Content-type'	 :   'text/xml; charset="utf-8"',
 				'SOAPACTION'	 :   '"urn:' + TvUrn + TvAction +'"'
 			},
-			body	: body }, function ( err , response , body ) {
+			body	: body 
+		}, function ( err , response , body ) {
 
 				if ( response.statusCode = 200 ) {
-
     				if ( data.key != undefined ) {
     					var regex = /<CurrentVolume>(\d*)<\/CurrentVolume>/gm;
 						var match = regex.exec ( body );
 						if (  match ) {
 							var volume = match[1].toString();
 							console.log ( "Volume TV = "+volume );
-							callback ({ 'tts': 'Le volume actuel est de' + volume + ' %'});
+							callback ({ 'tts': 'Le volume actuel est de' + volume + ' %' });
 						} else {
 							callback ({ 'tts': data.ttsAction });
 						}
